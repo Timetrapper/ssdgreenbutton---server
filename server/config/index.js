d0f568015a2752fc556b6f96a4cb14619bcbfaf1
Daniel <Timetrapper@users.noreply.github.com>
2018-04-26 10:42:32 -0700

    cleaned up config index

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,6 @@
 module.exports = {
-    // Database connection information
-  'database_mlab': 'mongodb://bcit:bcit123@ds249269.mlab.com:49269/plasmatic',
-  'database_local': 'greenbutton',
-  //'database': process.env.MONGODB_URI || 'mongodb://WillB:Kaya2015@ds231749.mlab.com:31749/boatsdb',
-  //exports.DATABASE_URL = process.env.MONGODB_URI || 'mongodb://localhost/local-app';
+  // Setting mlab database name
+  'database': 'mongo ds259109.mlab.com:59109/greenbuttondata -u bcitssd -p Bcit123!',
 
   // Setting port for server
   'port': process.env.PORT || 3000, 
@@ -13,9 +10,6 @@ module.exports = {
 
   // Setting mongo port number
   'mongo_port': process.env.MONGO_PORT || 27017,
-
-  // Setting mongo database name
-  'database': 'mongo ds259109.mlab.com:59109/greenbuttondata -u bcitssd -p Bcit123!',
 
   'secret': 'super secret passphrase',
 };
