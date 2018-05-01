be1f2ea8c036e8dc88840da162661d5b930a6c78
minathr <mina.thr@gmail.com>
2018-04-30 20:53:46 -0700

    added daily, weekly and monthly api

--- server/models/account.js
+++ server/models/account.js
@@ -81,16 +81,55 @@ module.exports.getUser = function(callback) {
     console.log('Done');
 };
 
-module.exports.getAccountHourlyUsage = function(callback) { 
+module.exports.getAccountHourlyUsage = function(id, callback) { 
     Account.aggregate([
-        {$unwind:"$feed"},
-        {$match: { "feed.id": "2"}},
+        {$unwind: "$feed"},
+        {$match: { "feed.id": id}}, 
         {$unwind:"$feed.entries"},
         {$match: { "feed.entries.title": "Interval Block - 1"}},
         {$unwind:"$feed.entries.content"},
         {$unwind:"$feed.entries.content.IntervalBlock"},
         {$unwind:"$feed.entries.content.IntervalBlock.IntervalReadings"},
-        {$match: { "feed.entries.content.IntervalBlock.IntervalReadings.timePeriod.start": {$gte: "1493175601", $lt:"1493190001"}}},
+        {$match: { "feed.entries.content.IntervalBlock.IntervalReadings.timePeriod.start": {$gte: 1521172801, $lt:1521187201}}},
         {$project: { Intervals: "$feed.entries.content.IntervalBlock.IntervalReadings" }}
-    ]);
+    ], function(err, data){
+        if (err)
+            throw err
+            console.log(data);
+        callback(null, data);
+    });
 };
+
+module.exports.getAccountAggregatedUsage = function(begin, end, callback){
+    Account.aggregate([
+        {$unwind:"$feed"},
+        {$match: { "feed.id": "Michael:uuid:79f97f63-967b-474b-bf10-e3d4b47531e1" }},
+        {$unwind:"$feed.entries"},
+        {$match: { "feed.entries.title": "Interval Block - 1"}},
+        {$unwind:"$feed.entries.content"},
+        {$unwind:"$feed.entries.content.IntervalBlock"},
+        {$unwind:"$feed.entries.content.IntervalBlock.IntervalReadings"},
+        {$match: { "feed.entries.content.IntervalBlock.IntervalReadings.timePeriod.start": {$gte: begin, $lt: end}}},
+        {$group: { 
+            _id: "$_id",
+            duration: {$sum: "$feed.entries.content.IntervalBlock.IntervalReadings.timePeriod.duration"},
+            start: {$min: "$feed.entries.content.IntervalBlock.IntervalReadings.timePeriod.start"},
+            value: {$sum: "$feed.entries.content.IntervalBlock.IntervalReadings.value"}
+        }} ,
+        {$project: {
+            intervalReading:{
+                timePeriod:{
+                    duration: "$duration",
+                    start: "$start"
+                },
+                value: "$value"
+            }
+
+        }}
+    ], function(err, data){
+        if (err)
+            throw err
+            console.log(data);
+        callback(null, data);
+    });
+}
--- server/routes/api/daily.js
+++ server/routes/api/daily.js
@@ -1,34 +1,71 @@
 var express = require('express');
 var router = express.Router();
 var config = require('../../config');
+var moment = require('moment');
 
-var mongojs = require("mongojs");
-//var db = mongojs(config.database_mlb, ["data"]);
-var db = mongojs(config.database_mlb, ["greenbuttondatas"]);
+//var mongojs = require("mongojs");
+//var db = mongojs(config.database, ["greenbuttondata"]);
 
 var Account = require("../../models/account");
 //var IntervalReading = require("../../models/intervalReading");
 
-router.get("/", function(req, res){
-    db.greenbuttondatas.aggregate([
-        {$unwind: "$data"},
-        {$unwind:"$data.feed"},
-        {$match: { "data.feed.id": "urn:uuid:79f97f63-967b-474b-bf10-e3d4b47531e1"}},
-        {$unwind:"$data.feed.entries"},
-        {$match: { "data.feed.entries.title": "Interval Block - 1"}},
-        {$unwind:"$data.feed.entries.content"},
-        {$unwind:"$data.feed.entries.content.IntervalBlock"},
-        {$unwind:"$data.feed.entries.content.IntervalBlock.IntervalReadings"},
-        {$match: { "data.feed.entries.content.IntervalBlock.IntervalReadings.timePeriod.start": {$gte: "1521057601", $lt:"1521064801"}}},
-        {$project: { Intervals: "$data.feed.entries.content.IntervalBlock.IntervalReadings" }}
-    ],
-    function(err, data){
+router.get("/hourly/:id", function(req, res){
+    console.log("in the function");
+    console.log("id: "+ req.params.id);
+    Account.getAccountHourlyUsage(req.params.id, function(err, data) {
+        console.log('here again');
         if (err)
             res.send(err);
         console.log("this is data: "+ JSON.stringify(data));
         res.json(data);
     });
 });
+
+router.get("/daily/:unixtime", function(req, res){
+
+    var beginOfDay = (moment.unix(req.params.unixtime).startOf('day'))/1000;
+    var endOfDay   = (moment.unix(req.params.unixtime).endOf('day'))/1000;
+    console.log('beginning of the Day: ' + beginOfDay);
+    console.log('end of the Day: ' + endOfDay);
+
+    Account.getAccountAggregatedUsage(beginOfDay, endOfDay, function(err, data){
+        if (err)
+            res.send(err);
+        console.log("this is data: "+ JSON.stringify(data));
+        res.json(data);
+    });
+});
+
+router.get("/weekly/:unixtime", function(req, res){
+
+    var beginOfWeek = (moment.unix(req.params.unixtime).utc().startOf('week'))/1000;
+    var endOfWeek   = (moment.unix(req.params.unixtime).utc().endOf('week'))/1000;
+    console.log('beginning of the Week: ' + beginOfWeek);
+    console.log('end of the Week: ' + endOfWeek);
+
+    Account.getAccountAggregatedUsage(beginOfWeek, endOfWeek, function(err, data){
+        if (err)
+            res.send(err);
+        console.log("this is data: "+ JSON.stringify(data));
+        res.json(data);
+    });
+});
+
+router.get("/monthly/:unixtime", function(req, res){
+
+    var beginOfMonth = (moment.unix(req.params.unixtime).utc().startOf('month'))/1000;
+    var endOfMonth   = (moment.unix(req.params.unixtime).utc().endOf('month'))/1000;
+    console.log('beginning of the Month: ' + beginOfMonth);
+    console.log('end of the Month: ' + endOfMonth);
+
+    Account.getAccountAggregatedUsage(beginOfMonth, endOfMonth, function(err, data){
+        if (err)
+            res.send(err);
+        console.log("this is data: "+ JSON.stringify(data));
+        res.json(data);
+    });
+});
+
 
 router.get("/user", function(req, res){
     Account.getUser(function(err, data){
