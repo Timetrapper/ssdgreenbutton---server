685216092838d3624363f604886406a6700142d5
minathr <mina.thr@gmail.com>
2018-05-10 16:01:52 -0700

    resolved conflicts

--- server/models/user.js
+++ server/models/user.js
@@ -28,8 +28,7 @@ module.exports.createUser = function(newUser, callback) {
             newUser.save(callback);
         });
     });
-<<<<<<< HEAD
-}
+};
 
 module.exports.addDataID = function(user, dataID, callback) {
     console
@@ -40,34 +39,19 @@ module.exports.addDataID = function(user, dataID, callback) {
             console.log(JSON.stringify(data));
         },
         callback);
-}
-=======
-  };
->>>>>>> 5399d044f4d3c1c4fa0124a4c422a6139217409b
+};
   
 module.exports.getUserByEmail = function(email, callback) {
     User.findOne({email: email}, callback);
-<<<<<<< HEAD
-} 
-=======
-  };
->>>>>>> 5399d044f4d3c1c4fa0124a4c422a6139217409b
+}; 
   
 module.exports.getUserById = function(id, callback) {
     User.findById(id, callback); 
-<<<<<<< HEAD
-}
-=======
-  };
->>>>>>> 5399d044f4d3c1c4fa0124a4c422a6139217409b
+};
   
 module.exports.comparePassword = function(candidatePassword, hash, callback) {
     bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
         if (err) throw err;
         callback(null, isMatch);
     });
-<<<<<<< HEAD
-}
-=======
-  };
->>>>>>> 5399d044f4d3c1c4fa0124a4c422a6139217409b
+};
